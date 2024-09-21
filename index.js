9ceb98556fb65b6c3b781c414e8fca0298c67eee
Abdullahi <aadahiye@gmail.com>
2024-09-21 09:38:34 +0300

    Completes arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+let num1 = 31;  
+let num2 = 2;   
 
-// Write your code here
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 100) + 1; 
+let num3 = 14;  
+let num4 = 10;  
+const mod = num3 % num4;
+const max = Math.max(5, 10, 15, 20, 3); 
